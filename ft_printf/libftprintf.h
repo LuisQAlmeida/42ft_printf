968e8bea94544d6f80422064a1001e1f84d269c1
Luís Quental Almeida <luis.quental.almeida@gmail.com>
2025-05-19 12:04:41 +0100

    Add libftprintf.h (initial draft)

---
+++ ft_printf/libftprintf.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmanuel- <lmanuel-@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/08 17:20:05 by lmanuel-          #+#    #+#             */
+/*   Updated: 2025/05/13 15:35:38 by lmanuel-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+# include <stdarg.h>
+
+int	ft_printf(const char *format, ...);
+#endif

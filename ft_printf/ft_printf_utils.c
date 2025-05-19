37628b92703493b08dc38fc039f11b7b3c190df0
Luís Quental Almeida <luis.quental.almeida@gmail.com>
2025-05-19 15:35:55 +0100

    Add function definitions for ft_strlen, ft_putchar and ft_putstr

--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -1,4 +1,34 @@
 
 #include "libftprintf.h"
 
-// File to include helper functions for printing and length calcs
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (*s)
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+}

76341ad299318489a0ead0a9b272efbdcaf686c4
Luís Quental Almeida <luis.quental.almeida@gmail.com>
2025-05-26 16:28:35 +0100

    Update ft_printf.c to version that only takes into consideration format specifiers

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -2,6 +2,12 @@
 #include "ft_printf.h"
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdio.h>
+
+int     ft_putchar_adptd(const char c)
+{
+        return(write(1, &c, 1));
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -10,21 +16,14 @@ int	ft_printf(const char *format, ...)
 
 	print_len = 0;
 	va_start(arg_list, format);
-	if (!format)
-	{
-		write(1, "(null)", 6);
-		return (-1);
-	}
 	while (*format)
 	{
-		if (*format == '%')
-		{
-			format++;
-		}
+		if (*format != '%')
+			print_len += ft_putchar_adptd(*format);
 		else
 		{
-			write(1, format, 1);
-			print_len++;
+			format++;
+			print_len += ft_print_format(*format, arg_list);
 		}
 		format++;
 	}
@@ -34,21 +33,9 @@ int	ft_printf(const char *format, ...)
 
 int	main(void)
 {
-	return (ft_printf(0));
-}
+	int	return_val;
 
-/*
-- Create helper functions to print:
-	- maybe use adapted ft_putnbr_base for p, d, i, u, x and X...
-		- For p, if we pass a NULL pointer, it should print "(nill)"
-		- For s, if we pass aNULL str, it should print "(null)"
-	- use write for c
-	- use ft_putstr for s
-- Create helper function to check what comes after %
-	- if it's a flag ('+', ' ' or '#') ->
-	- if it's a '-' or a number -> check next idexes until there are no more numbers..
-		- save the number in order to print the correct amount of spaces.
-	- if it's a '.' -> check next indexes for nothing or for numbers until """""..
-		- save the """""".
-	- if it's cspdiuxX% use funstions to print (ft_putstr, ft_putnbr...)
-*/
+	return_val = ft_printf("Hello\n");
+	printf("%d\n", return_val);
+	return (0);
+}

c73386a919a84d68a9333c852c05b3b6606a8c74
Luís Quental Almeida <luis.quental.almeida@gmail.com>
2025-05-26 16:29:51 +0100

    Update ft_printf_format.c to version that only takes into consideration format specifiers

--- ft_printf/ft_printf_format.c
+++ ft_printf/ft_printf_format.c
@@ -1,4 +1,26 @@
 
 #include "ft_printf.h"
 
-// File to include functions for format parsing
+int	ft_print_format(const char c, va_list arg_list)
+{
+	int	print_len;
+
+	print_len = 0;
+	if (c == 'c')
+		print_len += ft_putchar_adptd(va_arg(arg_list, int));
+	else if (c == 's')
+		print_len += ft_putstr_adptd(va_arg(arg_list, char *));
+	else if (c == 'd' || c == 'i')
+		print_len += ft_itoa_base_adptd(va_arg(arg_list, int), i);
+	else if (c == 'u')
+		print_len += ft_itoa_base_adptd(va_arg(arg_list, unsigned int), u);
+	else if (c == 'x')
+		print_len += ft_itoa_base_adptd(va_arg(arg_list, unsigned int), x);
+	else if (c == 'X')
+		print_len += ft_itoa_base_adptd(va_arg(arg_list, unsigned int), X);
+	else if (c == 'p')
+		print_len += ft_print_ptr(va_arg(arg_list, ksdfhksdgskdgfnsdkfgndkgndkfjgndkbn), p)
+	else
+		print_len += ft_putchar_adptd(c)
+	return (print_len);
+}

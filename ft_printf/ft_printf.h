5eec66e2d1cebbd51afabb4ea3d6fef8844b7ac2
Luís Quental Almeida <luis.quental.almeida@gmail.com>
2025-05-27 14:54:27 +0100

    Delete prototypes for ft_strlen, ft_putchar, ft_putstr. Add prototypes for ft_putchar_adptd, ft_putstr_apdtd, ft_putnbr_base_adptd, ft_printf_format

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -5,8 +5,9 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-int		ft_printf(const char *format, ...);
-size_t	ft_strlen(const char *s);
-void	ft_putchar(char c);
-void	ft_putstr(char *s);
+int	ft_printf(const char *format, ...);
+int	ft_putchar_adptd(const char c);
+int	ft_putstr_adptd(const char *s);
+int	ft_putnbr_base_adptd(const char *s, char c);
+int	ft_print_format(const char c, va_list arg_list);
 #endif

abfb1270163f70a5baf9940a5eb8c68dd66e35fe
Luís Quental Almeida <luis.quental.almeida@gmail.com>
2025-05-27 16:40:56 +0100

    Update prototype of ft_putnbr_base_adptd

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -8,6 +8,6 @@
 int	ft_printf(const char *format, ...);
 int	ft_putchar_adptd(const char c);
 int	ft_putstr_adptd(const char *s);
-int	ft_putnbr_base_adptd(const char *s, char c);
+int	ft_putnbr_base_adptd(const char *s, const char c);
 int	ft_print_format(const char c, va_list arg_list);
 #endif

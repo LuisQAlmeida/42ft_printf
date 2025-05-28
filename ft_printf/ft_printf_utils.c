bf7978fd4eb28182f84fe2ede0d37a5e6f7a3e35
Luís Quental Almeida <luis.quental.almeida@gmail.com>
2025-05-28 11:12:31 +0100

    Add function definition for ft_putnbr_base_adptd

--- ft_printf/ft_printf_utils.c
+++ ft_printf/ft_printf_utils.c
@@ -24,7 +24,30 @@ int	ft_putstr_adptd(const char *s)
 	return (i);
 }
 
-int	ft_putnbr_base_adptd(const char *s, const char c)
+int	ft_putnbr_base_adptd(unsigned long long num, char format)
 {
-
+	char	*base;
+	int		base_len;
+	int		print_len = 0;
+	if (format == 'p')
+		print_len += ft_putstr_adptd("0x");
+	if (format == 'X')
+		base = "0123456789ABCDEF";
+	if (format == 'x')
+		base = "0123456789abcdef";
+	if (format == 'd' || format == 'i' || format == 'u')
+		base_len = 10;
+	else
+		base_len = 16;
+	if (format == 'd' || format == 'i') {
+		int n = (int)num;
+		if (n < 0) {
+			print_len += ft_putchar_adptd('-');
+			num = -(long)n;
+		}
+	}
+	if (num >= (unsigned long long)base_len)
+		print_len += ft_putnbr_base_adptd(num / base_len, format);
+	print_len += ft_putchar_adptd(base[num % base_len]);
+	return (print_len);
 }

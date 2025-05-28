76e27258bbd6dbf3a9c83689e72e4efb4d0af2d7
Luís Quental Almeida <luis.quental.almeida@gmail.com>
2025-05-28 14:42:51 +0100

    Change name ft_putchar_adptd to ft_putchar_a, ft_putstr_adptd to ft_putstr_a, ft_putnbr_base_adptd to ft_putnum (To respect norm)

--- ft_printf/ft_printf_format.c
+++ ft_printf/ft_printf_format.c
@@ -1,4 +1,3 @@
-
 #include "ft_printf.h"
 
 int	ft_print_format(const char c, va_list arg_list)
@@ -7,22 +6,22 @@ int	ft_print_format(const char c, va_list arg_list)
 
 	print_len = 0;
 	if (c == 'c')
-		print_len += ft_putchar_adptd(va_arg(arg_list, int));
+		print_len += ft_putchar_a(va_arg(arg_list, int));
 	else if (c == 's')
-		print_len += ft_putstr_adptd(va_arg(arg_list, char *));
+		print_len += ft_putstr_a(va_arg(arg_list, char *));
 	else if (c == 'd' || c == 'i')
-		print_len += ft_putnbr_base_adptd(va_arg(arg_list, int), 'i');
+		print_len += ft_putnum(va_arg(arg_list, int), 'i');
 	else if (c == 'u')
-		print_len += ft_putnbr_base_adptd(va_arg(arg_list, unsigned int), 'u');
+		print_len += ft_putnum(va_arg(arg_list, unsigned int), 'u');
 	else if (c == 'x')
-		print_len += ft_putnbr_base_adptd(va_arg(arg_list, unsigned int), 'x');
+		print_len += ft_putnum(va_arg(arg_list, unsigned int), 'x');
 	else if (c == 'X')
-		print_len += ft_putnbr_base_adptd(va_arg(arg_list, unsigned int), 'X');
+		print_len += ft_putnum(va_arg(arg_list, unsigned int), 'X');
 	else if (c == 'p')
-		print_len += ft_putnbr_base_adptd((unsigned long)va_arg(arg_list, void *), 'p');
+		print_len += ft_putnum((unsigned long)va_arg(arg_list, void *), 'p');
 	else if (c == '%')
-		print_len += ft_putchar_adptd('%');
+		print_len += ft_putchar_a('%');
 	else
-		print_len += ft_putchar_adptd(c);
+		print_len += ft_putchar_a(c);
 	return (print_len);
 }

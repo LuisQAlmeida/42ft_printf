637470441c85073a7580ea8ed6e628a88f190725
Luís Quental Almeida <luis.quental.almeida@gmail.com>
2025-06-02 23:08:25 +0100

    Delete print_len and return printed length values directly to main function (ft_printf)

--- ft_printf/ft_printf_format.c
+++ ft_printf/ft_printf_format.c
@@ -2,26 +2,22 @@
 
 int	ft_print_format(const char c, va_list arg_list)
 {
-	int	print_len;
-
-	print_len = 0;
 	if (c == 'c')
-		print_len += ft_putchar_a(va_arg(arg_list, int));
+		return (ft_putchar_a(va_arg(arg_list, int)));
 	else if (c == 's')
-		print_len += ft_putstr_a(va_arg(arg_list, char *));
+		return (ft_putstr_a(va_arg(arg_list, char *)));
 	else if (c == 'd' || c == 'i')
-		print_len += ft_putnum(va_arg(arg_list, int), 'i');
+		return (ft_putnum(va_arg(arg_list, int), 'i'));
 	else if (c == 'u')
-		print_len += ft_putnum(va_arg(arg_list, unsigned int), 'u');
+		return (ft_putnum(va_arg(arg_list, unsigned int), 'u'));
 	else if (c == 'x')
-		print_len += ft_putnum(va_arg(arg_list, unsigned int), 'x');
+		return (ft_putnum(va_arg(arg_list, unsigned int), 'x'));
 	else if (c == 'X')
-		print_len += ft_putnum(va_arg(arg_list, unsigned int), 'X');
+		return (ft_putnum(va_arg(arg_list, unsigned int), 'X'));
 	else if (c == 'p')
-		print_len += ft_putnum((unsigned int)va_arg(arg_list, void *), 'p');
+		return (ft_putnum((unsigned int)va_arg(arg_list, void *), 'p'));
 	else if (c == '%')
-		print_len += ft_putchar_a('%');
+		return (ft_putchar_a('%'));
 	else
-		print_len += ft_putchar_a(c);
-	return (print_len);
+		return (ft_putchar_a(c));
 }
